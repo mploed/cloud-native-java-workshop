b2ddd5d04dd171ce014131ad914fd622b1904f8f
mploed <michael.ploed@gmail.com>
2017-05-07 12:49:51 +0200

    first project layout

---
+++ final-solution/credit-application/src/main/java/com/innoq/cloudnative/CreditApplication.java
@@ -0,0 +1,12 @@
+package com.innoq.cloudnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplication.class, args);
+	}
+}
